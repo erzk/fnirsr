245e409bed4c0e97ac0397000f981ee08a7ac0a6
Eryk Walczak <e.walczak@hotmail.com>
2017-04-15 23:27:57 +0100

    single channel detrending

--- R/detrend_ETG4000.R
+++ R/detrend_ETG4000.R
@@ -2,34 +2,53 @@
 #'
 #' Removes a linear trend.
 #'
-#' @param x Raw csv file.
+#' @param x A data frame with an ETG-4000 data.
+#' @param type A string. Specifies the type of plot. Allowed: "all" or "single".
+#' @param channel A numeric or string. If type is "single" then it has to be specified.
 #'
 #' @return A data frame.
 #' @export
 #'
 #' @examples
 #' file_path <- system.file("extdata", "Hitachi_ETG4000_24Ch_Total.csv", package = "fnirsr")
+#'
 #' fnirs_signal <- load_ETG4000_data(file_path)
 #' plot_ETG4000(fnirs_signal)
-#' fnirs_det <- detrend_ETG4000_data(fnirs_signal)
+#' fnirs_det <- detrend_ETG4000_data(fnirs_signal) # detrend all channels
 #' plot_ETG4000(fnirs_det)
 #'
-#' plot_ETG4000(fnirs_signal, "separate", 18)
+#' plot_ETG4000(fnirs_signal, "separate", 18) # zoom on one channel to notice detrending
 #' plot_ETG4000(fnirs_det, "separate", 18)
 #'
-detrend_ETG4000_data <- function(x) {
+#' fnirs_det_24 <- detrend_ETG4000_data(fnirs_signal, "single", 24) # detrend only one channel
+#' plot_ETG4000(fnirs_det_24, "separate", 24)
+#'
+detrend_ETG4000_data <- function(x, type = "all", channel = NULL) {
   if (!is.data.frame(x))
     stop("Please provide a data frame with ETG-4000 data.")
-  # detrend
-  df_CH <- select(x, starts_with("CH"))
-  df_CH <- apply(df_CH, 2, detrend)
-  # replace
-  CH_ind <- grepl("CH", names(x))
-  x[, CH_ind] <- df_CH
-  return(x)
-
-  # TODO add an option to detrend a single channel
-  # detrend channel 18: works
-  # det_fnirs <- detrend(fnirs_signal$CH18)
-  # plot(det_fnirs, type = "l")
+  if (!(type %in% c("all", "single"))) {
+    stop("Please how many channels to detrend: 'all' or 'single'.")
+  }
+  # detrend all channels
+  if (type == "all") {
+    # detrend
+    df_CH <- select(x, starts_with("CH"))
+    df_CH <- apply(df_CH, 2, detrend)
+    # replace
+    CH_ind <- grepl("CH", names(x))
+    x[, CH_ind] <- df_CH
+    return(x)
+  }
+  # detrend one channel
+  if (type == "single") {
+    if (missing(channel)) {
+      stop("Channel number to detrend is missing.")
+    } else {
+    CH_number <- paste0("CH", channel)
+    df_CH <- x[, CH_number]
+    df_CH <- detrend(df_CH)
+    x[, CH_number] <- df_CH
+    return(x)
+    }
+  }
 }

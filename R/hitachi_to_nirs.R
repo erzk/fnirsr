e494d2f362419d1242aff2baca6b8d5369e9a97c
Eryk Walczak <e.walczak@hotmail.com>
2017-04-16 16:48:04 +0100

    import/examples added

--- R/hitachi_to_nirs.R
+++ R/hitachi_to_nirs.R
@@ -1,5 +1,7 @@
 #' Load the data from a nirs file
 #'
+#' @importFrom tools file_ext
+#'
 #' @param x A Hitachi fNIRS file.
 #'
 #' @return A .nirs file.
@@ -8,7 +10,8 @@
 #' @references \url{https://www.nitrc.org/projects/hitachi2nirs/}
 #'
 #' @examples
-#' hitachi_to_nirs("....csv")
+#' file_path <- system.file("extdata", "Hitachi_ETG4000_24Ch_Total.csv", package = "fnirsr")
+#' hitachi_to_nirs(file_path)
 #'
 hitachi_to_nirs <- function(x) {
   if (file_ext(x) != "csv")

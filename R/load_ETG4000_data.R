a802ec853486f0b258417fbd1edc3ae140a79578
Eryk Walczak <e.walczak@hotmail.com>
2017-04-30 13:33:13 +0100

    dynamic skip

--- R/load_ETG4000_data.R
+++ R/load_ETG4000_data.R
@@ -1,5 +1,6 @@
 #' Load the ETG-4000 data
 #'
+#' @importFrom readr read_lines
 #' @importFrom tools file_ext
 #' @importFrom utils read.csv
 #' @importFrom stringr str_extract
@@ -20,7 +21,15 @@ load_ETG4000_data <- function(x, header_ETG4000) {
     stop("ETG4000 files should have a csv extension.")
   if (!is.list(header_ETG4000))
     stop("Please provide a valid ETG4000 header.")
-  dat <- read.csv(x, skip = 40) # TODO check if the number of rows varies
+  # find the line when data starts
+  input <- "start"
+  last_line <- 0L
+  while (!grepl("Data", input)) {
+    last_line <- last_line + 1L
+    input <- read_lines(file_path, skip = last_line - 1L, n_max = 1L)
+  }
+
+  dat <- read.csv(x, skip = last_line)
   # read the sampling period from the header
   sampling_element <- which(grepl("Sampling Period", header_ETG4000))
   sampling_element <- header_ETG4000[[sampling_element]]

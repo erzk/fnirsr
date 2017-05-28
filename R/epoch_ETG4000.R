12a2f81592d6eb6fd63a394d89dccee56420cac2
Eryk Walczak <e.walczak@hotmail.com>
2017-05-28 11:31:38 +0100

    abs and averaging added

--- R/epoch_ETG4000.R
+++ R/epoch_ETG4000.R
@@ -1,5 +1,7 @@
 #' Epoch the ETG-4000 data
 #'
+#' @import dplyr
+#'
 #' @param x A data frame with an ETG-4000 data.
 #' @param trigger A code used to represent a trigger, e.g. 1.
 #' @param start_epoch A numeric. Number of samples before the trigger. Start of the epoch.
@@ -13,13 +15,26 @@
 #' ETG_header <- load_ETG4000_header(file_path)
 #' rawData <- load_ETG4000_data(file_path, ETG_header)
 #'
-#' df_epochs <- epoch_ETG4000(rawData, 1, 100, 100)
+#' df_epochs <- epoch_ETG4000(rawData, 1, -100, 100)
 #'
 epoch_ETG4000 <- function(x, trigger, start_epoch, end_epoch) {
   if (!is.data.frame(x))
     stop("Please provide a data frame with ETG-4000 data.")
+  start_epoch <- abs(start_epoch)
+  end_epoch <- abs(end_epoch)
+
   trigger_indices <- which(x$Mark == trigger) # get the indices
+  # TODO check the space between the triggers
   rows <- lapply(trigger_indices, function(x) (x - start_epoch):(x + end_epoch))
   x <- x[unlist(rows),]
-  return(x)
+
+  sampled_epochs <- start_epoch + end_epoch + 1
+  channels_number <- length(names(select(df, starts_with("CH")))) + 1
+  x$EpochSample <- 1:sampled_epochs
+  x %>%
+    select(starts_with("CH"), Mark, EpochSample) %>%
+    group_by(EpochSample) %>%
+    summarise_each(funs(mean), 1:channels_number) %>%
+    select(-EpochSample) %>% # TODO add Time
+    return()
 }

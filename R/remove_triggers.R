6156e0298bcdc097a7758451ad6a821565debf6d
Eryk <e.walczak@hotmail.com>
2018-04-14 15:58:14 +0100

    every_other added

--- R/remove_triggers.R
+++ R/remove_triggers.R
@@ -1,7 +1,20 @@
 #' Remove triggers from the ETG-4000 data
 #'
+#' Replaces triggers/markings from ETG-4000 files as specified by the users.
+#' This functions can replace all triggers or every other trigger. The latter
+#' prepares the data frame to be loaded into Homer2.
+#'
+#' @importFrom dplyr group_by
+#' @importFrom dplyr mutate
+#' @importFrom dplyr row_number
+#' @importFrom dplyr select
+#' @importFrom magrittr %>%
+#'
 #' @param x A data frame with an ETG-4000 data.
 #' @param trigger An integer representign a trigger/marking to remove.
+#' @param n A character describing which trigger to remove.
+#' Allowed options: 'every' (default - removed all triggers) and 'every_other'
+#' (removes every other trigger - to prepare ETG-4000 data for loading into Homer2).
 #'
 #' @return A data frame.
 #' @export
@@ -10,17 +23,26 @@
 #' file_path <- system.file("extdata", "Hitachi_ETG4000_24Ch_Total.csv", package = "fnirsr")
 #' ETG_header <- load_ETG4000_header(file_path)
 #' ETG_data <-load_ETG4000_data(file_path, ETG_header)
-#' ETG_data_cleaned <- remove_triggers(ETG_data, 1)
+#' remove_triggers(ETG_data, 1)
+#' remove_triggers(ETG_data, 1, "every_other")
 #'
-remove_triggers <- function(x, trigger = NULL) {
+remove_triggers <- function(x, trigger = NULL, n = "every") {
   if (!is.data.frame(x))
     stop("Please provide a data frame with ETG-4000 data.")
   if (missing(trigger))
     stop("Please provide a trigger/mark) to be removed.")
   if (!("Mark" %in% names(x)))
     stop("There is no Mark column in the provided data frame.")
-  if (length(which(x$Mark == trigger)) == 0) {
+  if (length(which(x$Mark == trigger)) == 0)
     stop("Provided trigger is not in the data set.")
+  if (!(n %in% c("every", "every_other")))
+    stop("Specify the trigger to remove: 'every' or 'every_other'.")
+  if (n == "every_other")  {
+    x <- x %>% group_by(Mark) %>% mutate(Count = row_number()) %>% data.frame()
+    double_marks <- which(x$Mark == trigger & x$Count %% 2 == 0)
+    x$Mark[double_marks] <- 0
+    x <- subset(x, select = -Count)
+    return(x)
   } else {
   trigger_pos <- which(x$Mark == trigger)
   x[trigger_pos,] <- 0
--- tests/testthat/test-remove_triggers.R
+++ tests/testthat/test-remove_triggers.R
@@ -8,12 +8,16 @@ test_that("Removing triggers works correctly", {
   df <- load_ETG4000_data(file_path, header)
   df_trim <- df[, 1:3]
   df_rm1 <- remove_triggers(df, 1)
+  df_rm2 <- remove_triggers(df, 1, "every_other")
 
   expect_that(remove_triggers(header,1), throws_error())
   expect_that(remove_triggers(df), throws_error())
   expect_that(remove_triggers(df, 2), throws_error())
   expect_that(remove_triggers(df_trim, 1), throws_error())
+  expect_that(remove_triggers(df_trim, 1, "test"), throws_error())
 
   expect_that(df_rm1, is_a("data.frame"))
+  expect_that(df_rm2, is_a("data.frame"))
   expect_equal(unique(df_rm1$Mark), 0)
+  expect_equal(unique(df_rm2$Mark), c(0, 1))
 })
